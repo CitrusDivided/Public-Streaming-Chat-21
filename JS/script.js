7e71d3c6ad79d97eed4eff47100eab79f8a7caa4
CitrusDivided <78776670+CitrusDivided@users.noreply.github.com>
2025-01-15 14:18:10 -0800

    Further establishing text expansion;

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+const textarea = document.getElementById('expandingTextarea');
+textarea.addEventListener('input', () => {
+  textarea.style.height = 'auto'; // Reset the height
+  textarea.style.height = textarea.scrollHeight + 'px'; // Set it to the scrollHeight
+});
