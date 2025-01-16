a77eb00a8987efaffc5ba837bb9fd5dc08eee8dd
CitrusDivided <78776670+CitrusDivided@users.noreply.github.com>
2025-01-15 16:49:14 -0800

    Temp removal of js;

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,8 @@
-const textarea = document.getElementById('expandingTextarea');
-textarea.addEventListener('input', () => {
-  textarea.style.height = 'auto'; // Reset the height
-  textarea.style.height = textarea.scrollHeight + 'px'; // Set it to the scrollHeight
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const textarea = document.getElementById('expandingTextarea');
+//     textarea.addEventListener('input', () => {
+//       textarea.style.height = 'auto'; // Reset the height
+//       textarea.style.height = `${textarea.scrollHeight}px`; // Set it to the scrollHeight
+//     });
+//   });
+  
