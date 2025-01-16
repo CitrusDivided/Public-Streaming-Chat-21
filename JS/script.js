49e96b90606915306719e2b1a6ba746980d57c45
CitrusDivided <78776670+CitrusDivided@users.noreply.github.com>
2025-01-16 14:45:22 -0800

    Reverting changes;

--- JS/script.js
+++
@@ -1,8 +0,0 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const textarea = document.getElementById('expandingTextarea');
-//     textarea.addEventListener('input', () => {
-//       textarea.style.height = 'auto'; // Reset the height
-//       textarea.style.height = `${textarea.scrollHeight}px`; // Set it to the scrollHeight
-//     });
-//   });
-  
